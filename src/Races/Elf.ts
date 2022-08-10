e98e385437617cff0c0c2ade0c831c377beb9a99
Kaio G <kaiogreco91@gmail.com>
2022-08-10 18:20:33 -0300

    feat(Subclasses): cria as subclasses Dwarf, Elf, Halfling e Orc
    que herdam da superclasse Race. Além disso, faz ajuste no arquivo index

---
+++ src/Races/Elf.ts
@@ -0,0 +1,23 @@
+import Race from './Race';
+
+export default class Elf extends Race {
+  static _createdIntances = 0;
+  private _maxLifePoints: number;
+
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    super(name, dexterity);
+    this._maxLifePoints = 99;
+    Elf._createdIntances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return this._createdIntances;
+  }
+}
